75c455f5c2991c9cbc5eafec85b78f00e9d2fb28
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2022-05-30 09:51:23 +0200

    Neu Upload

---
+++ my-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './app.js'
+import './App.css';
+importScripts
+
+
+function App() {
+  return (
+    <div className="script">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/script.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
